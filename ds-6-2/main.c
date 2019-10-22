216d83b4f3a13a5802568370f05feaac53e85d52
Tao2564548730 <2564548730@qq.com>
2019-10-22 16:25:57 +0800

    Initial Commit

---
+++ ds-6-2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ds-6-2
+//
+//  Created by W20181105881 on 2019/10/22.
+//  Copyright © 2019 W20181105881. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
